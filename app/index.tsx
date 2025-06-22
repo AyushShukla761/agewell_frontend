0f67c6b179b4e808da3e388736dd99b2cbc0ca83
AyushShukla761 <ce23b057@smail.iitm.ac.in>
2025-06-22 15:35:55 +0530

    check

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 export default function Index() {
+  const router= useRouter()
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-2xl font-bold text-blue-600">
@@ -9,7 +11,22 @@ export default function Index() {
       <Text className="text-gray-600 mt-4">
         NativeWind is working with Expo Router
       </Text>
-      <Link href="/senior">Seniors</Link>
+      <TouchableOpacity
+                className="p-4 flex-1 mr-2"
+                style={{
+                  backgroundColor: "#d5dbda",
+                  borderRadius: 12,
+                  borderWidth: 1,
+                  borderColor: "#d5dbda",
+                }}
+                onPress={() => router.push("/senior")}
+                activeOpacity={0.8}
+              >
+                <View className="flex-row items-center justify-between">
+                  <Text className="text-black font-bold text-base">Learspace</Text>
+                  <Ionicons name="school" size={20} color="white" />
+                </View>
+              </TouchableOpacity>
       
     </View>
   );
